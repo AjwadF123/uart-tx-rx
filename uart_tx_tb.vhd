ccef2bcea92e3e90209305175ac4ee3020612bf4
AjwadF123 <95583428+AjwadF123@users.noreply.github.com>
2021-12-06 19:57:57 +0500

    Update uart_tx_tb.vhd

--- uart_tx_tb.vhd
+++ uart_tx_tb.vhd
@@ -1,29 +1,11 @@
 --------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
+
 -- Create Date:   11:13:58 12/04/2021
--- Design Name:   
--- Module Name:   D:/XilinxProjects/uart_tx_tb.vhd
 -- Project Name:  uart_tx
 -- Target Device:  
 -- Tool versions:  
 -- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: uart_tx
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
+
 --------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
